02476692d14207356b251c8bdb0a07747a29081c
Do Quang Giap <167546581+giapdq04@users.noreply.github.com>
2024-09-14 11:38:35 +0700

    đã sửa lại giao diện cho cân đối

--- app/src/main/java/com/example/globalmoney/MainScreen.kt
+++ app/src/main/java/com/example/globalmoney/MainScreen.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -39,7 +42,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.globalmoney.service.CurrencyReq
-import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
@@ -63,12 +65,12 @@ fun MainScreen(viewModelApp: ViewModelApp = viewModel()) {
     var isExpanded2 by remember { mutableStateOf(false) }
     var selectedChoice by remember {
         mutableStateOf(
-            listOf("")
+            listOf("Chọn nước")
         )
     }
     var selectedChoice2 by remember {
         mutableStateOf(
-            listOf("")
+            listOf("Chọn nước")
         )
     }
     val context = LocalContext.current
@@ -76,10 +78,10 @@ fun MainScreen(viewModelApp: ViewModelApp = viewModel()) {
     LaunchedEffect(Unit) {
         viewModelApp.codeViewModel()
         loading = false
-        code?.let {
-            selectedChoice = it.supported_codes[0]
-            selectedChoice2 = it.supported_codes[1]
-        }
+//        code?.let {
+//            selectedChoice = it.supported_codes[0]
+//            selectedChoice2 = it.supported_codes[1]
+//        }
     }
 
     LaunchedEffect(currency, loading) {
@@ -110,104 +112,137 @@ fun MainScreen(viewModelApp: ViewModelApp = viewModel()) {
                 // tiêu đề
                 Text("Chuyển đổi tiền tệ", fontSize = 30.sp, fontWeight = FontWeight.Bold)
 
-                // mệnh giá cần chuyển đổi
-                TextField(
-                    value = firstCurrency,
-                    onValueChange = {
-                        firstCurrency = it
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number,
-                        imeAction = ImeAction.Done
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            onClick()
-                            keyboardController?.hide() // khi bấm done sẽ dóng bàn phím
-                        }
-                    )
-                )
-                ExposedDropdownMenuBox(
-                    expanded = isExpanded,
-                    onExpandedChange = {
-                        isExpanded = !isExpanded
-                        Log.e("TAG", "code: " + code?.supported_codes!![0][0])
-                    },
-                ) {
-                    TextField(
-                        modifier = Modifier
-                            .menuAnchor()
-                            .size(width = 150.dp, height = 50.dp),
-                        value = selectedChoice[0],
-                        onValueChange = {},
-                        readOnly = true,
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) }
-                    )
+                Spacer(modifier = Modifier.height(20.dp))
 
-                    ExposedDropdownMenu(
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    ExposedDropdownMenuBox(
                         expanded = isExpanded,
-                        onDismissRequest = { isExpanded = false }
+                        onExpandedChange = {
+                            isExpanded = !isExpanded
+                            Log.e("TAG", "code: " + code?.supported_codes!![0][0])
+                        },
                     ) {
-                        code?.supported_codes?.forEachIndexed { index, s ->
-                            DropdownMenuItem(
-                                text = {
-                                    Text(s[1], modifier = Modifier.padding(start = 20.dp))
-                                },
-                                onClick = {
-                                    selectedChoice = s
-                                    isExpanded = false
-                                    Toast.makeText(
-                                        context,
-                                        "selectedChoice: $selectedChoice",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                },
-                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
-                            )
+                        TextField(
+                            modifier = Modifier
+                                .menuAnchor()
+                                .width(150.dp),
+                            value = selectedChoice[0],
+                            onValueChange = {},
+                            readOnly = true,
+                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) }
+                        )
+
+                        ExposedDropdownMenu(
+                            expanded = isExpanded,
+                            onDismissRequest = { isExpanded = false }
+                        ) {
+                            code?.supported_codes?.forEachIndexed { index, s ->
+                                DropdownMenuItem(
+                                    text = {
+                                        Text(s[1], modifier = Modifier.padding(start = 20.dp))
+                                    },
+                                    onClick = {
+                                        selectedChoice = s
+                                        isExpanded = false
+                                        Toast.makeText(
+                                            context,
+                                            "Đã chọn đơn vị tiền tệ của ${selectedChoice[1]}: ${selectedChoice[0]}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    },
+                                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                                )
+                            }
                         }
                     }
-                }
-
-                // mệnh giá sau khi chuyển đổi
-                Text(
-                    (if (convertedAmount % 1 == 0f) convertedAmount.toInt()
-                        .toString() else convertedAmount.toString()) + " VND",
-                    fontSize = 25.sp
-                )
-                ExposedDropdownMenuBox(
-                    expanded = isExpanded2,
-                    onExpandedChange = {
-                        isExpanded2 = !isExpanded2
-                    },
-                ) {
+                    // mệnh giá cần chuyển đổi
                     TextField(
-                        modifier = Modifier
-                            .menuAnchor()
-                            .size(width = 150.dp, height = 50.dp),
-                        value = selectedChoice2[0],
-                        onValueChange = {},
-                        readOnly = true,
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded2) }
+                        value = firstCurrency,
+                        onValueChange = {
+                            firstCurrency = it
+                        },
+                        modifier = Modifier.width(200.dp),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                onClick()
+                                keyboardController?.hide() // khi bấm done sẽ dóng bàn phím
+                            }
+                        )
                     )
+                }
 
-                    ExposedDropdownMenu(
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    ExposedDropdownMenuBox(
                         expanded = isExpanded2,
-                        onDismissRequest = { isExpanded2 = false }
+                        onExpandedChange = {
+                            isExpanded2 = !isExpanded2
+                        },
                     ) {
-                        code?.supported_codes?.forEachIndexed { index, s ->
-                            DropdownMenuItem(
-                                text = {
-                                    Text(s[1], modifier = Modifier.padding(start = 20.dp))
-                                },
-                                onClick = {
-                                    selectedChoice2 = s
-                                    isExpanded2 = false
-                                },
-                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
-                            )
+                        TextField(
+                            modifier = Modifier
+                                .menuAnchor()
+                                .width(150.dp),
+                            value = selectedChoice2[0],
+                            onValueChange = {},
+                            readOnly = true,
+                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded2) }
+                        )
+
+                        ExposedDropdownMenu(
+                            expanded = isExpanded2,
+                            onDismissRequest = { isExpanded2 = false }
+                        ) {
+                            code?.supported_codes?.forEachIndexed { index, s ->
+                                DropdownMenuItem(
+                                    text = {
+                                        Text(s[1], modifier = Modifier.padding(start = 20.dp))
+                                    },
+                                    onClick = {
+                                        selectedChoice2 = s
+                                        isExpanded2 = false
+                                        Toast.makeText(
+                                            context,
+                                            "Đã chọn đơn vị tiền tệ của ${selectedChoice2[1]}: ${selectedChoice2[0]}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    },
+                                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                                )
+                            }
                         }
                     }
+                    // mệnh giá sau khi chuyển đổi
+                    Box(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(56.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            (if (convertedAmount % 1 == 0f) convertedAmount.toInt()
+                                .toString() else convertedAmount.toString()) + " VND",
+                            fontSize = 25.sp
+                        )
+                    }
+
                 }
+
 
                 Text(
                     "Cập nhật lần cuối: $lastUpdate",
